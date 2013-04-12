415c8d321eb79aa49617134c0a487f5febe6bcbe
Vincent <pich.2@hotmail.fr>
2013-04-12 14:23:09 +0200

    enlever les truk de merde

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,16 +16,7 @@ public class Main {
 		interf.fileFromPath("csvFiles/gen-02.csv");
 		
 		interf.showHumans(interf.getHumans(), interf.getLinesFromFile());
-		JFrame windows = new JFrame();
-		//Définit un titre pour notre fenêtre
-		windows.setTitle("Tree");
-		//Définit sa taille : 400 pixels de large et 100 pixels de haut
-		windows.setSize(400, 100);
-		//Nous demandons maintenant à notre objet de se positionner au centre
-		windows.setLocationRelativeTo(null);
-		//Termine le processus lorsqu'on clique sur la croix rouge
-		windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		windows.setVisible(true);
+	
 	}
 
 }
