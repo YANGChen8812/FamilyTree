e7f96e9eddc8c72f58d15d3244ce106712031325
choupinette <guyomar@et.esiea.fr>
2013-04-29 16:58:57 +0200

    Delete useless import + Text JLabel

--- src/game/Menu.java
+++ src/game/Menu.java
@@ -4,13 +4,16 @@ package game;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+
+
+
 
 
 public class Menu extends JFrame {
@@ -22,15 +25,14 @@ public class Menu extends JFrame {
 
 	public Menu (){
 		JFrame fen =new JFrame();
+		fen=new JFrame(" MENU ");
 		fen . setPreferredSize (new Dimension (400 ,480));
 		fen . setDefaultCloseOperation ( JFrame . EXIT_ON_CLOSE );
 		fen . setLayout (new BorderLayout ());
-		JPanel topPanel = new JPanel ();
-		topPanel . setLayout ( new FlowLayout ( FlowLayout . LEFT ));
-		fen .add( topPanel , BorderLayout . NORTH );
+		
 		JPanel centerPanel = new JPanel ();
 		centerPanel . setLayout ( new GridLayout (4 ,1));
-		centerPanel . add( new JTextField ("Qui est-ce?"));
+		centerPanel . add( new JLabel ("Qui est-ce?"));
 		centerPanel . add( new JButton ("Règles" ));
 		centerPanel . add( new JButton ("Jouer" ));
 		centerPanel . add( new JButton ("Quitter" ));
@@ -38,8 +40,4 @@ public class Menu extends JFrame {
 		fen . pack ();
 		fen . setVisible ( true );
 		}
-		
-	}
-
-	
-
+}
