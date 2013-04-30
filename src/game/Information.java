6efa92e2e4631005103f3b80a6075db2847e8a16
choupinette <guyomar@et.esiea.fr>
2013-04-30 16:28:03 +0200

    Add commentary to create a ScrollPane

--- src/game/Information.java
+++ src/game/Information.java
@@ -3,13 +3,17 @@
 
 	// import java.awt.BorderLayout;
 	import java.awt.BorderLayout;
-	import java.awt.Dimension;
-	import java.awt.FlowLayout;
+ //import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 	import javax.swing.JButton;
-	import javax.swing.JFrame;
-	import javax.swing.JLabel;
-	import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+//import javax.swing.JScrollPane;
+//import javax.swing.JTextField;
+// import javax.swing.ScrollPaneConstants;
 
 	public class Information extends JFrame {
 
@@ -30,15 +34,23 @@
 			topPanel.setLayout( new FlowLayout ( FlowLayout.CENTER)); 
 			topPanel.add(new JLabel("Information"));
 			
-			fen.add(topPanel, BorderLayout. CENTER ); 
+			fen.add(topPanel, BorderLayout. NORTH ); 
 			
 			JPanel centerPanel = new JPanel ();
 			centerPanel . setLayout ( new FlowLayout ( FlowLayout . CENTER ));
 			fen .add( centerPanel , BorderLayout . SOUTH );
 			centerPanel . add( new JButton ("Retour" ));
 			centerPanel . add( new JButton ("Valider" ));
+		    //JScrollPane Info = new JScrollPane();
+		    
+		   
+		   // setVertcialScrollBar(JScrollBar)
+			//JTextField content = new JTextField (" jhjhupfchjnzkjkiiiiiiiiiiiikkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk ");
 			
-			
+			//Info.add(content);
+			//fen.getContentPane().add( Info, BorderLayout.CENTER);
+		 
+		    
 			fen . pack ();
 			fen . setVisible ( true );
 		}
